c4ff30d091656201da8088b6c6f4b34dfcb3d870
ᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠᅠ <w3bsmes@gmail.com>
2021-09-07 06:44:51 +0300

    /

--- packages/app/index.ts
+++ packages/app/index.ts
@@ -1,28 +1,69 @@
 import { v4 as uuidv4 } from "uuid";
 import puppeteer from "puppeteer";
+import fetch from "node-fetch";
 
-type Count = number;
+import { log } from "./utils";
+
+import { Count, Certificate } from "./types";
 
 /**
- * Count of SS
+ * Count of screenshots
  *
  * @var {Number}
  */
-const count: Count = 100;
+const count: Count = 10;
 
-(async () => {
-  const browser = await puppeteer.launch({
-    defaultViewport: { width: 300, height: 300 },
-  });
-  const page = await browser.newPage();
-  for (let i = 0; i < count; i++) {
-    await page.goto(
-      `https://www.gosuslugi.ru/vaccine/cert/verify/${uuidv4()}`,
-      { waitUntil: "networkidle2" }
-    );
-    await page.screenshot({
-      path: `${__dirname}/${uuidv4()}.jpeg`,
-    });
+export const getCertInfo = async (
+  idCertificate: string
+): Promise<Certificate | undefined> => {
+  const response = await fetch(
+    `https://www.gosuslugi.ru/api/vaccine/v2/cert/verify/${idCertificate}`
+  );
+
+  if (!response.ok || response.status === 204) {
+    return;
   }
-  await browser.close();
-})();
+
+  return response.json() as Promise<Certificate>;
+};
+
+getCertInfo("117d099c-fe34-42f8-8ddd-bf47f79590c5")
+  .then((response) => {
+    return response;
+  })
+  .catch((err) => err);
+
+// (async () => {
+//   const browser = await puppeteer.launch({
+//     defaultViewport: { width: 300, height: 300 },
+//   });
+//   const page = await browser.newPage();
+
+//   for (let i = 0; i < count; i++) {
+//     const value = uuidv4();
+
+//     try {
+//       const response = await fetch(
+//         `https://www.gosuslugi.ru/api/vaccine/v2/cert/verify/${value}`
+//       );
+
+//       if (response.ok) {
+//         log(response.status, response.statusText /*, data*/);
+//         if (response.status !== 204) {
+//           await page.goto(
+//             `https://www.gosuslugi.ru/vaccine/cert/verify/${value}`,
+//             {
+//               waitUntil: "networkidle2",
+//             }
+//           );
+//           await page.screenshot({
+//             path: `${__dirname}/${value}.jpeg`,
+//           });
+//         }
+//       }
+//     } catch (error) {
+//       if (error) log("pizdec");
+//     }
+//   }
+//   await browser.close();
+// })();
